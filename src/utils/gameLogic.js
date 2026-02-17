a40cde0231f88d5a8a40b5c4a7d262dbcb8a55a7
David <daveedtee5@gmail.com>
2026-02-17 14:58:13 -0500

    TEST

--- src/utils/gameLogic.js
+++ src/utils/gameLogic.js
@@ -2,6 +2,8 @@
 export const SUITS = ['♠', '♥', '♦', '♣'];
 export const VALUES = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
 
+console.log("TESST")
+
 export function createDeck() {
   const deck = [];
   for (const suit of SUITS) {
